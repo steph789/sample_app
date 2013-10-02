42408a52221111dd29494a58c629db6893bc21e4
steph789 <stephanie.gerlich@gmail.com>
2013-10-02 19:51:14 +0200

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
+
+root to: "static_pages#home"
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
