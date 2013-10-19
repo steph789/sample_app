97f43d14df3c61d3244fbdb6cf344a1174572355
steph789 <stephanie.gerlich@gmail.com>
2013-10-19 18:16:50 +0200

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
 	def show
 	  	@user = User.find(params[:id])
@@ -11,6 +13,7 @@ class UsersController < ApplicationController
 	def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       # Handle a successful save.
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -18,6 +21,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 
   private
 
